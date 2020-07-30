3d018b62eb7f2c085b290f8c84c61e51f0d5a4ad
MelinaBeltran <melibeltran735@hotmail.com>
2020-07-30 08:24:53 -0300

    ordenadores

---
+++ MetodosDeOrdenamiento.h
@@ -0,0 +1,110 @@
+//Metodo de ordenamiento por Bubuja Mejorada
+void Burbuja(double v1[],int n){
+	int b=0,aux=0;
+	do{   
+		b=0;   
+		for(int i=0;i<(n-1);i++){    
+			if(v1[i]>v1[i+1]){     
+				aux=v1[i];     
+				v1[i]=v1[i+1];     
+				v1[i+1]=aux;     
+				b=1;    
+			}   
+		}  
+	}while(b==1);    
+} 
+//Metodo de ordenamiento por Insercion
+void Insercion(double v2[],int n){
+	int posicion=0,aux=0,i=0;
+	for(int i=0; i<n; i++){
+        posicion=i;
+        aux=v2[i];
+        while((posicion>0) && (v2[posicion-1]>aux)){
+            v2[posicion] = v2[posicion -1];
+            posicion--;
+        }
+        v2[posicion] = aux;
+    }
+}
+//Metodo de ordenamiento por Seleccion
+void Seleccion(double v3[],int n){
+	int k=0,menor=0,j=0;
+	for(int i=0;i<n;i++){
+		menor=v3[i];
+		k=i;
+		for(j=i+1;j<n;j++){
+		    if(v3[j]<menor){
+		    	menor=v3[j];
+		        k=j;
+		    }
+		}
+		v3[k]=v3[i];
+		v3[i]=menor;
+    }
+}
+//Metodo de ordenamiento por Quicksort
+void Quicksort(double v4[],int izq,int der){
+	int i=izq;
+	int j=der,tmp;
+	double p=v4[(izq+der)/2];
+	while(i<=j){
+		while(v4[i]<p){
+			i++;
+		}
+		while(v4[j]>p){
+			j--;
+		}
+		if(i<=j){
+			tmp=v4[i];
+			v4[i]=v4[j];
+			v4[j]=tmp;
+			i++;
+			j--;
+		}
+	}
+	if(izq<j){
+		Quicksort(v4,izq,j);
+	}
+	if(i<der){
+		Quicksort(v4,i,der);
+	}
+}
+//Metodo de ordenamiento por Mezcla
+void Mezcla(double v5[],int n){
+	//Corta 
+	if(n==1){
+	 	return;
+	}
+ 	int mitad=(n/2);
+	double izq[mitad],der[n-mitad];
+ 	for(int i=0;i<mitad;i++){
+ 	 	izq[i]=v5[i];
+ 	}
+ 	for(int i=mitad;i<n;i++){
+  		der[i-mitad] = v5[i];
+  	}
+  	Mezcla(izq,mitad);
+  	Mezcla(der,n-mitad);
+ 	izq[0];der[0];v5[0];
+	int nIzq = mitad,nDer = (n-mitad),i=0,j=0,k=0;
+ 	while( ( i < nIzq ) && ( j < nDer )){
+  		if(izq[i] <= der[j]){
+   			v5[k] = izq[i];
+   			i++;
+  		}else{
+   			v5[k] = der[j];
+   			j++;
+  		}
+  		k++;
+ 	}
+ 	while(i < nIzq){
+  		v5[k] = izq[i];
+  		i++;
+  		k++;
+ 	}
+ 	while(j < nDer){
+ 		v5[k] = der[j];
+  		j++;
+  		k++;
+ 	}
+}
---
+++ Principal.cpp
@@ -0,0 +1,150 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+#include"MetodosDeOrdenamiento.h"
+int opc=0;
+double Duracion=0,Inicio=0,Fin=0;
+void MenuPrincipal();
+void CargarVector(double v1[],double v2[],double v3[],double v4[],double v5[],int n);
+int main(){
+	system("color 2");
+	int z=0,j=0,i=0,n=0,b=0,aux=0;
+	//pedimos el tamaño del vector
+	printf("\n Digite la cantidad de numeros que decea cargar: ");scanf("%d",&n);
+	//asignamos el tamaño al vector
+	double v1[n],v2[n],v3[n],v4[n],v5[n];
+	//llamamos la funcion
+	CargarVector(v1,v2,v3,v4,v5,n);
+	do{
+		//llamamos la funcion
+		MenuPrincipal();
+		switch(opc){
+			case 1:{
+				system("cls");
+				//Inicia calculando el tiempo del ordenador 
+			    Inicio=time(NULL);
+			    //llamamos el metodo de ordenamiento
+				Burbuja(v1,n);
+				//mostramos los vectores ordenados
+				for(int i=1;i<n;i++){
+					printf("%.0lf\t \t",v1[i]);
+				}
+				//Finaliza el tiempo calculado del ordenador
+				Fin=time(NULL);
+				//calculamos el tiempo empleado
+				Duracion=Fin-Inicio;
+				printf("\nTiempo empleado: %.2lf",Duracion);
+				getch();	
+	 		break;}
+	 		case 2:{
+	 			system("cls");
+	 			Duracion=0;Inicio=0;Fin=0;
+	 			//Inicia calculando el tiempo del ordenador 
+	 			Inicio=time(NULL);
+	 			//llamamos el metodo de ordenado
+	 			Insercion(v2,n);
+	 			//mostramos los vectores ordenados
+			    for(int i=0;i<n;i++){
+		            printf(" %.0lf\t \t",v2[i]);
+	            }
+				//Finaliza el tiempo calculado del ordenador
+				Fin=time(NULL);
+				//calculamos el tiempo empleado
+				Duracion=Fin-Inicio;
+				printf("\nTiempo empleado: %.2lf",Duracion);
+				getch();
+			break;}
+			case 3:{
+				system("cls");
+				Duracion=0;Inicio=0;Fin=0;
+				//Inicia calculando el tiempo del ordenador 
+				Inicio=time(NULL);
+				//llamamos el metodo de ordenado
+				Seleccion(v3,n);
+				//mostramos los vectores ordenados
+				for(int i=0;i<n;i++){
+					printf(" %.0lf\t \t",v3[i]);
+				}
+				//Finaliza el tiempo calculado del ordenador
+				Fin=time(NULL);
+				//calculamos el tiempo empleado
+				Duracion=Fin-Inicio;
+				printf("\nTiempo empleado: %.2lf",Duracion);
+				getch();	
+			break;}
+			case 4:{
+				system("cls");
+				Duracion=0;Inicio=0;Fin=0;
+				//Inicia calculando el tiempo del ordenador
+				Inicio=time(NULL);
+				//llamamos el metodo de ordenado
+				Quicksort(v4,0,n);
+				//mostramos los vectores ordenados	
+				for(int i=1;i<n;i++){
+					printf(" %.0lf\t \t",v4[i]);
+				}
+				//Finaliza el tiempo calculado del ordenador
+				Fin=time(NULL);
+				//calculamos el tiempo empleado
+				Duracion=Fin-Inicio;
+				printf("\nTiempo empleado: %.2lf",Duracion);
+				getch();
+			break;}	
+			case 5:{
+				system("cls");
+				Duracion=0;Inicio=0;Fin=0;
+				//Inicia calculando el tiempo del ordenador
+				Inicio=time(NULL);
+				//llamamos el metodo de ordenado
+				Mezcla(v5,n);
+				//mostramos los vectores ordenados	
+				for(int g=1;g<n;g++){
+					printf(" %.0lf\t \t",v5[g]);
+				}
+				//Finaliza el tiempo calculado del ordenador
+				Fin=time(NULL);
+				//calculamos el tiempo empleado
+				Duracion=Fin-Inicio;
+				printf("\nTiempo empleado: %.2lf",Duracion);
+				getch();
+			break;}
+			case 6:{
+				printf("FIN");		
+			break;}
+		}
+	}while(opc!=6);
+	getch();system("cls");
+}
+void CargarVector(double v1[],double v2[],double v3[],double v4[],double v5[],int n){
+	system("cls");
+	srand(time(NULL));
+	for(int i=0;i<n;i++){
+		//cargamos los vectores los vectores aleatorios
+		v1[i]=(1+(rand()%100))*i;
+		v2[i]+=v1[i];
+		v3[i]+=v1[i];
+		v4[i]+=v1[i];
+		v5[i]+=v1[i];
+		printf("%.0lf \n",v1[i]);
+	}
+	getch();
+}
+void MenuPrincipal(){
+	//Menu
+	system("cls");
+	system("color 2");
+	printf("\n                                                    MENU PRINCIPAL    ");
+	printf("\n----------------------------------------------------------------------------------------------------------------------------");
+	printf("\n    1.-Burbuja mejorado");
+	printf("\n\n    2.-Insercion");
+	printf("\n\n    3.-Metodo seleccion");
+	printf("\n\n    4.-QuickSort");
+	printf("\n\n    5.-Por mezcla");
+	printf("\n\n    6.-Salir");
+	printf("\n---------------------------------------------------------------------------------------------------------------------------");
+	printf("\n                                            **********************************");
+	printf("\n                                             SELECIONE EL NUMERO DE SU OPCION ");
+	printf("\n                                            ***********************************                                  opc ->> ");
+	scanf("%d",&opc);
+}
